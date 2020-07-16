978dc78d9cb954624c6911ed9380a7c234607a16
Felix Lublasser <felix.lublasser@fromatob.com>
2020-07-16 12:02:08 +0200

    add api URI namespace

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :players, only: %i[index create show]
+  namespace :api do
+    resources :players, only: %i[index create show]
 
-  resources :series, only: %i[show create index update destroy] do
-  	resources :games, only: %i[create index update]
+    resources :series, only: %i[show create index update destroy] do
+    	resources :games, only: %i[create index update]
+    end
   end
 end
