6aaa493ce8a605bbcf0af23e620d45d670015204
Felix Lublasser <felix.lublasser@fromatob.com>
2020-07-16 11:35:22 +0200

    stuff

--- config/application.rb
+++ config/application.rb
@@ -33,7 +33,7 @@ module Srv32schreibauf
     config.api_only = true
 
     config.action_dispatch.default_headers = {
-      'Access-Control-Allow-Origin' => 'http://localhost',
+      'Access-Control-Allow-Origin' => '*',
       'Access-Control-Request-Method' => %w{GET POST PUT}.join(",")
     }
   end
